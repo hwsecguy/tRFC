a4cdd173aafee93b76d8124641b7fc7d40e4d37f
hwsecguy <144949377+hwsecguy@users.noreply.github.com>
2023-10-13 13:19:29 -0400

    feat(hist.py): add funciton to specify input histogram

--- hist.py
+++ hist.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
+import sys
 
 # Read the file and split it into two lists: times and latencies
 times = []
 latencies = []
 
-with open('histogram.txt', 'r') as f:
+file_name = "histogram.txt"
+if len(sys.argv[1]) > 0:
+    file_name = sys.argv[1]
+
+with open(file_name, 'r') as f:
     for line in f:
         time, latency = line.split()
         times.append(int(time))
